506475212389a55b084364ae1a689d34923decd5
Matt <mluckydwyer@gmail.com>
2017-02-13 18:40:19 -0600

    Adding Shooter detection switch

--- IrishRobotics2017/src/org/usfirst/frc/team2606/robot/Robot.java
+++ IrishRobotics2017/src/org/usfirst/frc/team2606/robot/Robot.java
@@ -1,8 +1,5 @@
 package org.usfirst.frc.team2606.robot;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-
 import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.UsbCamera;
@@ -14,11 +11,6 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.vision.VisionThread;
-
-
-
-
-
 
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
@@ -39,11 +31,23 @@ public class Robot extends IterativeRobot {
 
 	public static int autonomousLoopCounter;
 	public static Command autonomousCommand;
+
+	// Default D-Pad Control Drive Speed 0 - 100%
+	private static double DEFAULT_DPAD_SPEED;
 	
+	// Shooter Hold Speed Steady Button
+	private static boolean isShooterHoldSteady;
+
+	// Default Shooter Speed 0 - 100%
+	private static double DEFAULT_MIN_SHOOTER_SPEED;
+
+	// Shooter Hold Steady Speed
+	private static double shooterHoldSteadySpeed;
+
 	// Size of camera image
 	private static int IMG_WIDTH;
 	private static int IMG_HEIGHT;
-	
+
 	// Center of Image & Thread Lock object to used when obtaining the image
 	private double centerX = 0.0;
 	private final Object imgLock = new Object();
@@ -61,64 +65,71 @@ public class Robot extends IterativeRobot {
 		autonomousRoutinePicker.addObject("Test 1", new AutonomousDriveForward());
 		autonomousRoutinePicker.addObject("Test 2", new AutonomousDriveForward());
 		SmartDashboard.putData("Autonomous Mode Command", autonomousRoutinePicker);
+
+		// Default D-Pad Drive Speed
+		DEFAULT_DPAD_SPEED = 0.5;
 		
+		// Shooter Default Speed
+		DEFAULT_MIN_SHOOTER_SPEED = 0.55;
+		isShooterHoldSteady = false;
+
 		// Vision Code
 		IMG_WIDTH = 640;
 		IMG_HEIGHT = 480;
-		
+
 		// Raw unchanged camera
 		UsbCamera cameraRaw = CameraServer.getInstance().startAutomaticCapture("Normal Camera", 0);
-    	cameraRaw.setResolution(IMG_WIDTH, IMG_HEIGHT);
-		
-    	// Reflective detection camera 
-		UsbCamera cameraReflective = CameraServer.getInstance().startAutomaticCapture("Reflective Camera Raw", 1);
-    	cameraReflective.setResolution(IMG_WIDTH, IMG_HEIGHT);
-    	
-    	// Set settings for ideal camera settings for best reflective detection
-    	//cameraReflective.setExposureManual(10);
-    	//cameraReflective.setExposureHoldCurrent();
-    	cameraReflective.setBrightness(0);
-    	
-    	// Put the Reflection images to the dashboard
-    	CvSource cvOutput = CameraServer.getInstance().putVideo("Reflective Detection Output", IMG_WIDTH, IMG_HEIGHT);
-    	CvSink cvSink = CameraServer.getInstance().getVideo(cameraReflective);
+		cameraRaw.setResolution(IMG_WIDTH, IMG_HEIGHT);
 
-    	// Create the Mat object to work with the output of the detection code
-    	Mat mat = new Mat();
-    	
-    	// Create the vision detection thread and get the output of each frame
-    	VisionThread visionThread = new VisionThread(cameraReflective, new GripPipeline(), pipeline -> {
-    		// Bounding Rectangle
-    		Rect r = new Rect();
-    		
-    		// Draw a rectangle around the detection and get the center
-    		if (!pipeline.convexHullsOutput().isEmpty()) {
-                //r = Imgproc.boundingRect(pipeline.convexHullsOutput().get(0));
-                synchronized (imgLock) {
-                    double total = 0;
-                    Mat m = new MatOfPoint(m)
-                	
-                	for(MatOfPoint p : pipeline.convexHullsOutput()) {
-                		r = r | Imgproc.boundingRect(p);
-                		total += r.x + (r.width / 2);
-                		centerX = total / pipeline.convexHullsOutput().size();
-                	}
-                	//centerX = r.x + (r.width / 2);
-               }
-            }
-    		
-    		// Get the processed frame and put it to the dashboard
-    		cvSink.grabFrame(mat);
-    		pipeline.process(mat);
-    		
-    		// Draw the rectangle and center of detection on the image
-    		Mat processedImage = pipeline.maskOutput();
-    		Imgproc.rectangle(processedImage, new Point(r.x, r.y), new Point(r.x + r.width, r.y + r.height), new Scalar(255, 0, 0), 1);
-    		Imgproc.drawContours(processedImage, pipeline.filterContoursOutput(), 0, new Scalar(0, 0, 255), 1);
-    		//cvOutput.putFrame(pipeline.maskOutput());
-    		cvOutput.putFrame(processedImage);
-    	});
-    	visionThread.start();
+		// Reflective detection camera
+		UsbCamera cameraReflective = CameraServer.getInstance().startAutomaticCapture("Reflective Camera Raw", 1);
+		cameraReflective.setResolution(IMG_WIDTH, IMG_HEIGHT);
+
+		// Set settings for ideal camera settings for best reflective detection
+		// cameraReflective.setExposureManual(10);
+		// cameraReflective.setExposureHoldCurrent();
+		cameraReflective.setBrightness(0);
+
+		// Put the Reflection images to the dashboard
+		CvSource cvOutput = CameraServer.getInstance().putVideo("Reflective Detection Output", IMG_WIDTH, IMG_HEIGHT);
+		CvSink cvSink = CameraServer.getInstance().getVideo(cameraReflective);
+
+		// Create the Mat object to work with the output of the detection code
+		Mat mat = new Mat();
+
+		// Create the vision detection thread and get the output of each frame
+		VisionThread visionThread = new VisionThread(cameraReflective, new GripPipeline(), pipeline -> {
+			// Bounding Rectangle
+				Rect r = new Rect();
+
+				// Draw a rectangle around the detection and get the center
+				if (!pipeline.convexHullsOutput().isEmpty()) {
+					// r =
+					// Imgproc.boundingRect(pipeline.convexHullsOutput().get(0));
+				synchronized (imgLock) {
+					double total = 0;
+
+					for (MatOfPoint p : pipeline.convexHullsOutput()) {
+						r = Imgproc.boundingRect(p);
+						total += r.x + (r.width / 2);
+						centerX = total / pipeline.convexHullsOutput().size();
+					}
+					// centerX = r.x + (r.width / 2);
+				}
+			}
+
+			// Get the processed frame and put it to the dashboard
+			cvSink.grabFrame(mat);
+			pipeline.process(mat);
+
+			// Draw the rectangle and center of detection on the image
+			Mat processedImage = pipeline.maskOutput();
+			Imgproc.rectangle(processedImage, new Point(r.x, r.y), new Point(r.x + r.width, r.y + r.height), new Scalar(255, 0, 0), 1);
+			Imgproc.drawContours(processedImage, pipeline.filterContoursOutput(), 0, new Scalar(0, 0, 255), 1);
+			// cvOutput.putFrame(pipeline.maskOutput());
+			cvOutput.putFrame(processedImage);
+		});
+		visionThread.start();
 	}
 
 	/**
@@ -191,63 +202,141 @@ public class Robot extends IterativeRobot {
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run(); // Run any called commands
 
-		// Drive Code
+		// ----- Drive Code -----
 
-		// B button pressed & Right Trigger is beyond Halfway pressed drive in reverse
-		if (RobotMap.xboxController.getRawButton(2)
-				&& RobotMap.xboxController.getRawAxis(3) > 0.5)
-			RobotMap.robotDrive.tankDrive(-1, -1);
+		// B button pressed & Right Trigger is beyond Halfway pressed drive in
+		// reverse
+		if (RobotMap.xboxController.getRawButton(2) && RobotMap.xboxController.getRawAxis(3) > 0.5)
+			RobotMap.robotDrive.tankDrive(1.0, 1.0);
 
 		// Drive full Speed Strait Forward
 		else if (RobotMap.xboxController.getRawButton(2)) // B button pressed
-			RobotMap.robotDrive.tankDrive(1, 1);
+			RobotMap.robotDrive.tankDrive(-1.0, -1.0);
 
-		// Drives Forward at full speed forward if Up on the D-Pad is pressed
+		// D-Pad Drive
+		
+		// Drives Forward at Default D-Pad Drive Speed speed forward if the D-Pad is pressed Up ^
 		else if (RobotMap.xboxController.getPOV() == 0)
-			RobotMap.robotDrive.tankDrive(1, 1);
+			RobotMap.robotDrive.tankDrive(-DEFAULT_DPAD_SPEED, -DEFAULT_DPAD_SPEED);
 
-		// Drives Forward at full speed backward if Down on the D-Pad is pressed
+		// Drives Forward at Default D-Pad Drive Speed speed forward and turns Left if the D-Pad is
+		// pressed Up and Left <^
+		else if (RobotMap.xboxController.getPOV() == 315)
+			RobotMap.robotDrive.tankDrive(-DEFAULT_DPAD_SPEED / 2, -DEFAULT_DPAD_SPEED);
+
+		// Drives Forward at Default D-Pad Drive Speed speed forward and turns Right if the D-Pad is
+		// pressed Up and Right ^>
+		else if (RobotMap.xboxController.getPOV() == 45)
+			RobotMap.robotDrive.tankDrive(-DEFAULT_DPAD_SPEED, -DEFAULT_DPAD_SPEED / 2);
+
+		// Turns Robot Right if the D-Pad is pressed Right >
+		else if (RobotMap.xboxController.getPOV() == 90)
+			RobotMap.robotDrive.tankDrive(-DEFAULT_DPAD_SPEED / 4, DEFAULT_DPAD_SPEED / 4);
+
+		// Drives Forward at Default D-Pad Drive Speed speed backward if Down on the D-Pad is pressed
+		// \/
 		else if (RobotMap.xboxController.getPOV() == 180)
-			RobotMap.robotDrive.tankDrive(-1, -1);
+			RobotMap.robotDrive.tankDrive(DEFAULT_DPAD_SPEED, DEFAULT_DPAD_SPEED);
+
+		// Drives Forward at Default D-Pad Drive Speed speed backward and turns Left if the D-Pad is
+		// pressed Down and Left <\/
+		else if (RobotMap.xboxController.getPOV() == 225)
+			RobotMap.robotDrive.tankDrive(DEFAULT_DPAD_SPEED / 2, DEFAULT_DPAD_SPEED);
+
+		// Drives Forward at Default D-Pad Drive Speed speed forward and turns Right if the D-Pad is
+		// pressed Up and Right \/>
+		else if (RobotMap.xboxController.getPOV() == 135)
+			RobotMap.robotDrive.tankDrive(DEFAULT_DPAD_SPEED, DEFAULT_DPAD_SPEED / 2);
+
+		// Turns Robot Left if the D-Pad is pressed Left <
+		else if (RobotMap.xboxController.getPOV() == 270)
+			RobotMap.robotDrive.tankDrive(DEFAULT_DPAD_SPEED / 4, -DEFAULT_DPAD_SPEED / 4);
 
 		// Drive With Triggers
-		// Left Trigger controls left wheels, Right Trigger controls right wheels
-		else if (RobotMap.xboxController.getRawAxis(2) != 0 || RobotMap.xboxController.getRawAxis(3) != 0)
-			// If the A button is held down the robot drives in reverse
-			if (RobotMap.xboxController.getRawButton(1)) RobotMap.robotDrive.tankDrive(RobotMap.xboxController.getRawAxis(3), RobotMap.xboxController.getRawAxis(2));
-			// If The A Button isn't held, it then goes forward
-			else RobotMap.robotDrive.tankDrive(-RobotMap.xboxController.getRawAxis(3), -RobotMap.xboxController.getRawAxis(2));
-		
+		// Left Trigger controls left wheels, Right Trigger controls right
+		// wheels
+		// else if (RobotMap.xboxController.getRawAxis(2) != 0 ||
+		// RobotMap.xboxController.getRawAxis(3) != 0)
+		// If the A button is held down the robot drives in reverse
+		// if (RobotMap.xboxController.getRawButton(1))
+		// RobotMap.robotDrive.tankDrive(RobotMap.xboxController.getRawAxis(3),
+		// RobotMap.xboxController.getRawAxis(2));
+		// If The A Button isn't held, it then goes forward
+		// else
+		// RobotMap.robotDrive.tankDrive(-RobotMap.xboxController.getRawAxis(3),
+		// -RobotMap.xboxController.getRawAxis(2));
+
+		// Drive With Bumbers
+		// Turns Robot Slightly Left if Right bumper is pressed
+		// if (RobotMap.xboxController.getRawButton(5))
+		// RobotMap.robotDrive.drive(1, -.5);
+
+		// Turns Robot Slightly Right if Right bumper is pressed
+		// else if (RobotMap.xboxController.getRawButton(6))
+		// RobotMap.robotDrive.drive(1, .5);
+
 		// Y Axis of Left joystick controls the Left wheels
 		// Y Axis of Right joystick controls the Right wheels
 		else
-			RobotMap.robotDrive.tankDrive(
-					RobotMap.xboxController.getRawAxis(5),
-					RobotMap.xboxController.getRawAxis(1));
+			RobotMap.robotDrive.tankDrive(RobotMap.xboxController.getRawAxis(5), RobotMap.xboxController.getRawAxis(1));
+
+		// ----- Climber Code -----
 
 		// Turns Robot Slightly Left if Right bumper is pressed
-		//if (RobotMap.xboxController.getRawButton(5))
-			//RobotMap.robotDrive.drive(1, -.5);
+		if (RobotMap.xboxController.getRawButton(5))
+			RobotMap.climberMotor.set(-1);
 
 		// Turns Robot Slightly Right if Right bumper is pressed
-		//else if (RobotMap.xboxController.getRawButton(6))
-			//RobotMap.robotDrive.drive(1, .5);
+		else if (RobotMap.xboxController.getRawButton(6))
+			RobotMap.climberMotor.set(1);
 
-		
-		// Climber Code
-		
-		// Turns Robot Slightly Left if Right bumper is pressed
-				if (RobotMap.xboxController.getRawButton(5))
-					RobotMap.climberMotor.set(-1);
+		// Stop Climber if no bumper is pressed
+		else
+			RobotMap.climberMotor.stopMotor();
 
-				// Turns Robot Slightly Right if Right bumper is pressed
-				else if (RobotMap.xboxController.getRawButton(6))
-					RobotMap.climberMotor.set(1);
-				
-				// Stop Climber if no bumper is pressed
-				else RobotMap.climberMotor.stopMotor();
+		// ----- Shooter Code -----
+
+		// Toggle Hold Steady Mode
+		if (RobotMap.xboxController.getRawButton(3)) {
+			isShooterHoldSteady = !isShooterHoldSteady;
+
+			// Hold Steady Code
+			if (isShooterHoldSteady) {
+
+				// Set the speed at which to hold the shooter motor at (%)
+
+				// Y-Button held and Trigger pressed; Set hold speed to reverse
+				// trigger speed
+				if (RobotMap.xboxController.getRawAxis(2) != 0 && RobotMap.xboxController.getRawButton(4))
+					shooterHoldSteadySpeed = DEFAULT_MIN_SHOOTER_SPEED + RobotMap.xboxController.getRawAxis(3) * 0.45;
+				// Y-Button held; Set hold speed to reverse default minimum
+				// speed
+				else if (RobotMap.xboxController.getRawButton(4))
+					shooterHoldSteadySpeed = DEFAULT_MIN_SHOOTER_SPEED;
+				// Trigger pressed; Set hold speed to forward trigger speed
+				else if (RobotMap.xboxController.getRawAxis(2) != 0)
+					shooterHoldSteadySpeed = DEFAULT_MIN_SHOOTER_SPEED + RobotMap.xboxController.getRawAxis(3) * 0.45;
+				// Set hold speed to forward default minimum speed
+				else
+					shooterHoldSteadySpeed = -DEFAULT_MIN_SHOOTER_SPEED;
+			}
+		}
+
+		// Set the speed of the Shooter Motor
+		// Y-Button held and Trigger pressed; Set hold speed to reverse trigger
+		// speed
+		if (RobotMap.xboxController.getRawAxis(2) != 0 && RobotMap.xboxController.getRawButton(4))
+			RobotMap.shooterMotor.set(DEFAULT_MIN_SHOOTER_SPEED + RobotMap.xboxController.getRawAxis(3) * 0.45);
+		// Trigger pressed; Set hold speed to forward trigger speed
+		else if (RobotMap.xboxController.getRawAxis(2) != 0)
+			RobotMap.shooterMotor.set(-DEFAULT_MIN_SHOOTER_SPEED - RobotMap.xboxController.getRawAxis(2) * 0.45);
+		// X-Button pressed/Hold Steady Mode On; Hold Shooter Speed Constant
+		else if (isShooterHoldSteady)
+			RobotMap.shooterMotor.set(shooterHoldSteadySpeed);
+		// Stop the motor
+		else
+			RobotMap.shooterMotor.set(0);
 	}
-
 	/**
 	 * This function is called periodically during test mode
 	 */
--- IrishRobotics2017/src/org/usfirst/frc/team2606/robot/RobotMap.java
+++ IrishRobotics2017/src/org/usfirst/frc/team2606/robot/RobotMap.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team2606.robot;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Spark;
@@ -15,17 +16,21 @@ import edu.wpi.first.wpilibj.Victor;
 public class RobotMap {
 	
 	// Xbox One Controller
-	public static Joystick xboxController = new Joystick(0);;
+	public static Joystick xboxController = new Joystick(0); // Xbox Controller Joystick Object
 	
 	// Drive Motor Speed Controllers
-	public static SpeedController frontLeftMotor = new Victor(0);
-	public static SpeedController frontRightMotor = new Victor(2);
-	public static SpeedController rearLeftMotor = new Victor(1);
-	public static SpeedController rearRightMotor = new Victor(3);
+	public static SpeedController frontLeftMotor = new Victor(0); // Front Left Drive Motor
+	public static SpeedController frontRightMotor = new Victor(2); // Front Right Drive Motor
+	public static SpeedController rearLeftMotor = new Victor(1); // Rear Left Drive Motor
+	public static SpeedController rearRightMotor = new Victor(3); // Rear Right Drie Motor
 	
 	// Special System Motor Speed Controllers
-	public static SpeedController climberMotor = new Spark(4); // Robot Climbing Motor Controller
-	public static SpeedController shooterMotor = new Spark(5); // Shooter Wheel Motor Controller
+	public static SpeedController feederMotor = new Victor(4); // Shooter Ball Feeding Motor Controller
+	public static SpeedController climberMotor = new Spark(5); // Robot Climbing Motor Controller
+	public static SpeedController shooterMotor = new Spark(6); // Shooter Wheel Motor Controller
+	
+	// Ball Shot Detection Switch
+	public static DigitalInput ballShotSwitch = new DigitalInput(0); // Detects and Fires A Signal When A Ball Is Shot
 	
 	public static RobotDrive robotDrive = new RobotDrive(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor); // Robot Drive Object
 }
